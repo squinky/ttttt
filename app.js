f81a5a8031ce1c6c2e5c5299b752938211aca82d
Squinky <squinky@Squinkputer.wireless.concordia.ca>
2016-11-10 16:28:31 -0500

    Keypresses enabled and also added some libraries

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ var SCREEN_PAYMENT = 3;
 var SCREEN_TROUBLESHOOT = 4;
 var SCREEN_ENDGAME = 5;
 
+var OBJECT_1 = 87; // w
+var OBJECT_2 = 68; // d
+
 var currentScreen;
+var objectsBeingTouched = [];
 
 function setCurrentScreen(s)
 {
@@ -72,6 +76,16 @@ function cycleCurrentScreen()
 	if (currentScreen == SCREEN_ENDGAME) setCurrentScreen(SCREEN_TITLE);
 	else setCurrentScreen(currentScreen+1);
 }
+
+$(document).keydown(function(e)
+{
+	objectsBeingTouched[e.keyCode] = true;
+	if (objectsBeingTouched[OBJECT_1] && objectsBeingTouched[OBJECT_2]) console.log("Touching both objects!")
+})
+.keyup(function(e)
+{
+	objectsBeingTouched[e.keyCode] = false;
+});
 
 $(document).ready(function()
 {
